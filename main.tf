fa68c40c33ba006eebb8a39440a7f9fb7d82b69c
sayedareef123 <103107800+sayedareef123@users.noreply.github.com>
2022-12-23 17:38:24 +0530

    commit

--- main.tf
+++ main.tf
@@ -9,13 +9,15 @@ variable "name" {
 }
 
 resource "tls_private_key" "private_key" {
+    account = 0
   algorithm = "RSA"
   rsa_bits = "2048"
 }
   
  resource "aws_key_pair" "key_pair" {
+    account = 0
   key_name = var.name
-  public_key = tls_private_key.private_key.public_key_openssh
+  public_key = tls_private_key.private_key.[0]public_key_openssh
   tags = {
     Name = var.name
   }
