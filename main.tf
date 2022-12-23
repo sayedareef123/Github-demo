d069a917ed1412d585f380c548f95e52372a601b
sayedareef123 <103107800+sayedareef123@users.noreply.github.com>
2022-12-23 13:40:51 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
 provider "aws" {
-  region = "us-east-2"   
+  region = "us-east-1"   
 }
 
 variable "name" {
     type = string
     description = "(optional) des variable"
-    default = "areef-tf"
+    default = "areefs"
 }
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
   rsa_bits = "2048"
 }
   
- resource "aws_key_pair" "key_pair" {
+ resource "aws_key_pair" "areef" {
   key_name = var.name
   public_key = tls_private_key.private_key.private_key_pem
   tags = {
     Name = var.name
   }
-}
-  
+ }
