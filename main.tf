7b429f583513e726e10575d951e0c5d6dc4ea7bd
sayedareef123 <103107800+sayedareef123@users.noreply.github.com>
2022-12-23 14:44:02 +0530

    commit

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ resource "tls_private_key" "private_key" {
   rsa_bits = "2048"
 }
   
- resource "aws_key_pair" "areef" {
+ resource "aws_key_pair" "private_key" {
   key_name = var.name
-  public_key = tls_private_key.private_key.private_key_pem
+  public_key = tls_private_key.private_key.public_key_openssh
   tags = {
     Name = var.name
   }
