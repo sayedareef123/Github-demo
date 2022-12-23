cad7f7eebbcc689be87587179c3cf4ee3f143456
sayedareef123 <103107800+sayedareef123@users.noreply.github.com>
2022-12-23 13:14:44 +0530

    added resource

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-1"   
 }
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits = "2048"
+}
+  
+ resource "aws_key_pair" "areef" {
+  key_name = var.name
+  public_key = tls_private_key.private_key.public_key_openssh
+  tags = {
+    Name = var.name
+  }
+}
+  
